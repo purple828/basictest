5c316a88199012a7e333c72dd00322a93912e551
fanglijuan <fanglijuan@baibu.la>
2019-03-20 16:51:54 +0800

    Initial commit

---
+++ src/main/java/com/example/basictest/BasictestApplication.java
@@ -0,0 +1,13 @@
+package com.example.basictest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasictestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasictestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/basictest/BasictestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.basictest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BasictestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
